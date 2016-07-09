1514a25cd09bf1fb006978064c432c40cdff31ba
Jason Hang <dev.jdhang@gmail.com>
2016-07-09 13:24:02 -0400

    Submission for deadline.

--- app.js
+++ app.js
@@ -21,7 +21,112 @@ app.use(session({
 }))
 
 // setting api routes
-app.use('/api', require('./routes/api.router.js'))
+app.use('/api', require('./routes/api.app.js'))
+
+app.param('/api/book/id', function (req, res, id) {
+  Book.findOne({ where: { id: id }})
+  .then((book) => {
+    if (book) {
+      req.requestedBook = book
+      next()
+    } else {
+      res.sendStatus(404)
+    }
+  })
+})
+
+app.get('/api/book', function (req, res) {
+  let options = {}
+  if (req.query) options = { where: req.query }
+  Book.findAll(options)
+  .then((books) => {
+    res.send(books)
+  })
+})
+
+app.post('/api/book', function (req, res) {
+  Book.create(req.body)
+  .then((createdBook) => {
+    res.status(201).send(createdBook)
+  })
+})
+
+app.get('/api/book/:id', function (req, res) {
+  res.send(req.requestedBook)
+})
+
+app.put('/api/book/:id', function (req, res) {
+  req.requestedBook.update(req.body)
+  .then((updatedBook) => {
+    res.send(updatedBook)
+  })
+})
+
+app.delete('/api/book/:id', function (req, res) {
+  req.requestedBook.destroy()
+  .then(() => {
+    res.sendStatus(204)
+  })
+})
+
+app.param('/api/book/:id/chapters/:chapterId', function (req, res, chapterId) {
+  Chapter.findOne({ where: { id: chapterId } })
+  .then((chapter) => {
+    if (chapter) {
+      req.requestedChapter = chapter
+      next()
+    } else {
+      res.sendStatus(404)
+    }
+  })
+})
+
+app.get('/api/book/:id/chapters/', function (req, res) {
+  req.requestedBook.getChapters()
+  .then((chapters) => {
+    res.send(chapters)
+  })
+})
+
+app.post('/api/book/:id/chapters/', function (req, res) {
+  Chapter.create(req.body)
+  .then((createdChapter) => {
+    return Promise.all([
+      req.requestedBook.addChapter(createdChapter),
+      createdChapter.setBook(req.requestedBook)
+    ])
+  })
+  .spread((book, chapter) => {
+    res.status(201).send(chapter)
+  })
+})
+
+app.get('/api/book/:id/chapters/:chapterId', function (req, res) {
+  res.send(req.requestedChapter)
+})
+
+app.put('/api/book/:id/chapters/:chapterId', function (req, res) {
+  req.requestedChapter.update(req.body)
+  .then((updatedChapter) => {
+    res.send(updatedChapter)
+  })
+})
+
+app.delete('/api/book/:id/chapters/:chapterId', function (req, res) {
+  req.requestedChapter.destroy()
+  .then(() => {
+    res.sendStatus(204)
+  })
+})
+
+app.get('/numVisits', function (req, res, next) {
+  if (req.session.views !== undefined) {
+    req.session.views++
+  } else{
+    req.session.views = 0
+  }
+  res.json({ number: req.session.views })
+})
 
 // handle internal server errors
 app.use('/broken', function (req, res, next) {
